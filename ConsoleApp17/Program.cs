7ec6d4380d1c97a0e07ed99af904428f6368312a
Alitahmaz <alirt@code.edu.az>
2022-12-20 21:17:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] birinciarray = { 1, 3, -5, 4 };
+            int[] ikinciarray = { 1, 4, -5, -2 };
+
+            Console.WriteLine("Array1: [{0}]", string.Join(", ", birinciarray));
+            Console.WriteLine("Array2: [{0}]", string.Join(", ", ikinciarray));
+
+            Console.WriteLine("iki elementin vurmasi: ");
+
+            for (int i = 0; i < birinciarray.Length; i++)
+            {
+
+                Console.Write(birinciarray[i] * ikinciarray[i] + " ");
+            }
+        }
+    }
+}
